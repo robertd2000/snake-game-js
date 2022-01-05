5844c62f875585dd542f47fef4e438b32254afc4
robert d <zdabaghov@mail.ru>
2022-01-05 18:25:31 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,43 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+import { outsideGrid } from './grid.js'
+
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime) {
+    if (gameOver) {
+        if (confirm('You lost. Press OK if you want to restart  ')) {
+            window.location = '/'
+        } 
+        return
+    }
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkForDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkForDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
